c566650048c482aa11c9328b4f61c914ef1984d5
Lennart Klose <stu222821@mail.uni-kiel.de>
2026-03-09 11:23:08 +0100

    added app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-# Empty for now
+import pandas as pd
+import dash
+from dash import dcc
+from dash import html
+from dash import callback, Input, Output
+import plotly.express as px
+
+
+
+app = dash.Dash(__name__)
+server = app.server
+
+app.layout = html.Div([
+    html.H1("This is a test"),
+    html.H3("This is a subheading"),
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
